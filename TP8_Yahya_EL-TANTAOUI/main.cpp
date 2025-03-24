86c28a9133e5ae0ca7532bd6ef1723b7140b513f
Yahya EL-TANTAOUI <150563585+ELTANTAOUI-Y7@users.noreply.github.com>
2025-03-24 01:52:51 +0000

    Add files via upload

---
+++ TP8_Yahya_EL-TANTAOUI/main.cpp
@@ -0,0 +1,16 @@
+#include "Movies.hpp"
+
+int main() {
+    Movies collection;
+
+    collection.add_movie("Big", "PG-13", 2);
+    collection.add_movie("Inception", "PG-13", 5);
+    collection.add_movie("The Matrix", "R", 7);
+
+    collection.display();
+
+    collection.increment_watched("Inception");
+    collection.display();
+
+    return 0;
+}
